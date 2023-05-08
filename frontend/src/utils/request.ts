a93373a2634e05961c3ea0e0aae00bdb8b93dae8
liuyu <russell44@126.com>
2023-05-08 12:43:37 +0800

    debug

--- frontend/src/utils/request.ts
+++ frontend/src/utils/request.ts
@@ -1,4 +1,4 @@
-const URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5174"
+const URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8080"
 export async function getImgList({
   test = false,
   exp = 1,
